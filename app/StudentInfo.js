98aa5aee5d017fdf81a85dc80be1444dd7054a9d
mudittpathak <mudit.pathak@edu.sait.ca>
2023-09-25 22:20:10 -0600

    commit

---
+++ app/StudentInfo.js
@@ -0,0 +1,17 @@
+
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Mudit Pathak</h1>
+      <h2>Course Section: CPRG 306 C</h2>
+      <p><Link href="https://github.com/mudittpathak">My GitHub Repository</Link></p>
+
+        
+      
+    </div>
+  );
+};
+
+export default StudentInfo;
